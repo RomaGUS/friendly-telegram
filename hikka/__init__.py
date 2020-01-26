1835c6552f80aa29f82db8a65e031dc3ebd677cb
volbil <volbilnexus@gmail.com>
2020-01-26 19:20:05 +0900

    Simple frontend for testing

--- hikka/__init__.py
+++ hikka/__init__.py
@@ -12,9 +12,5 @@ from hikka.modules import auth
 from hikka.modules import files
 
 @app.route("/")
-def home():
-    return render_template("home.html", title="Home page")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+def root():
+    return render_template("index.html")
