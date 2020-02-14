147b9f081463f6da89423ccc74aea086fa696b79
volbil <volbilnexus@gmail.com>
2020-02-15 02:51:17 +0900

    Move mongoengine connection to hikka module

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 from hikka import app
-import mongoengine
 import config
 
 if __name__ == "__main__":
-    db_settings = dict(
-        username=config.db["username"],
-        password=config.db["password"],
-        port=config.db["port"]
-    )
-
-    mongoengine.register_connection(
-        alias="default",
-        name=config.db["name"],
-        **db_settings
-    )
-
     app.run(
         debug=config.debug,
         host=config.host,
--- hikka/__init__.py
+++ hikka/__init__.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
 from flask_restful import Api
 from flask_cors import CORS
+import mongoengine
 import config
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = config.secret
 api = Api(app)
 CORS(app)
+
+db_settings = dict(
+    username=config.db["username"],
+    password=config.db["password"],
+    port=config.db["port"]
+)
+
+mongoengine.register_connection(
+    alias="default",
+    name=config.db["name"],
+    **db_settings
+)
 
 from hikka import routes
 
