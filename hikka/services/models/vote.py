48d46713a8601ade163df5877cc17021384581ef
volbil <volbilnexus@gmail.com>
2020-03-12 13:17:31 +0900

    Misc vote object change

--- hikka/services/models/vote.py
+++ hikka/services/models/vote.py
@@ -3,10 +3,10 @@ import mongoengine
 
 class Vote(mongoengine.Document):
     rating = mongoengine.IntField(min_value=1, max_value=10, required=True)
+    account = mongoengine.ReferenceField("User", required=True)
+    subject = mongoengine.GenericReferenceField(required=True)
     created = mongoengine.DateTimeField(default=datetime.now)
     updated = mongoengine.DateTimeField(default=datetime.now)
-    subject = mongoengine.GenericReferenceField()
-    account = mongoengine.ReferenceField("User")
 
     meta = {
         "alias": "default",
