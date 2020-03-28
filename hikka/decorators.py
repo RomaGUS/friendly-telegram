d69945730141936277ad2351d9293f9fb84297f6
volbil <volbilnexus@gmail.com>
2020-03-29 02:30:38 +0900

    Update user login time

--- hikka/decorators.py
+++ hikka/decorators.py
@@ -1,6 +1,7 @@
 from hikka.services.permissions import PermissionService
 from hikka.services.users import UserService
 from hikka.errors import abort
+from datetime import datetime
 from functools import wraps
 from flask import request
 
@@ -12,6 +13,9 @@ def auth_required(view_function):
             return abort("account", "login-failed")
 
         request.account = account
+        account.login = datetime.now()
+        account.save()
+
         return view_function(*args, **kwargs)
 
     return decorator
