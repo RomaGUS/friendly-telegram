c9d8cfc2b8f3d8a189aee083b9965a5d2fa9455c
volbil <volbilnexus@gmail.com>
2020-03-31 15:58:00 +0900

    Misc code cleanup

--- hikka/auth.py
+++ hikka/auth.py
@@ -1,70 +1,14 @@
 from datetime import datetime, timedelta
+from hikka.tools.jwt import JWT
 from hikka import utils
-import hashlib
 import config
 import bcrypt
-import base58
-import hmac
-import json
 
 def hashpwd(password: str) -> str:
     return bcrypt.hashpw(str.encode(password), bcrypt.gensalt()).decode()
 
 def checkpwd(password, bcrypt_hash) -> bool:
     return bcrypt.checkpw(str.encode(password), str.encode(bcrypt_hash))
-
-# https://gist.github.com/darelf/190bc97b29e91509534d7535ebde4762
-class JWT():
-    @classmethod
-    def create_signed_token(cls, key, data):
-        """
-        Create a complete JWT token. Exclusively uses blake2b
-        HMAC.
-        """
-        header = json.dumps({"typ": "JWT", "alg": "BLK2B"}).encode("utf-8")
-        henc = base58.b58encode_check(header).decode()
-
-        payload = json.dumps(data).encode("utf-8")
-        penc = base58.b58encode_check(payload).decode()
-
-        hdata = henc + "." + penc
-
-        d = hmac.new(key, hdata.encode("utf-8"), digestmod=hashlib.blake2b)
-        dig = d.digest()
-        denc = base58.b58encode_check(dig).decode()
-
-        token = hdata + "." + denc
-        return token
-
-    @classmethod
-    def verify_signed_token(cls, key, token):
-        """
-        Validate token HMAC signature.
-        """
-        try:
-            (header, payload, sig) = token.split(".")
-            hdata = header + "." + payload
-
-            d = hmac.new(key, hdata.encode("utf-8"), digestmod=hashlib.blake2b)
-            dig = d.digest()
-            denc = base58.b58encode_check(dig).decode()
-
-            return hmac.compare_digest(sig, denc)
-
-        except Exception:
-            return False
-
-    @classmethod
-    def decode_payload(cls, token):
-        """
-        Decodes the payload in the token and returns a dict.
-        """
-        try:
-            (header, payload, sig) = token.split(".")
-            return json.loads(base58.b58decode_check(payload).decode())
-
-        except Exception:
-            return {}
 
 class Token():
     @classmethod
