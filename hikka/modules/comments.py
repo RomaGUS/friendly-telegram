4471d9b9e0a6d43ea11844845a6063bbf6d35747
volbil <volbilnexus@gmail.com>
2020-04-19 23:25:37 +0900

    Convert comments into blueprint

--- hikka/modules/comments.py
+++ hikka/modules/comments.py
@@ -3,80 +3,81 @@ from hikka.services.anime import AnimeService
 from hikka.tools.parser import RequestParser
 from hikka.decorators import auth_required
 from datetime import datetime, timedelta
+from flask import request, Blueprint
 from flask.views import MethodView
 from hikka.tools import helpers
 from hikka.errors import abort
-from flask import request
 
+blueprint = Blueprint("comments", __name__)
 choices = ("anime")
 
 def get_service(service):
     if service == "anime":
         return AnimeService
 
-class NewComment(MethodView):
-    @auth_required
-    def post(self):
-        result = {"error": None, "data": {}}
+@blueprint.route("/comments/new", methods=["POST"])
+@auth_required
+def new_comment():
+    result = {"error": None, "data": {}}
 
-        parser = RequestParser()
-        parser.argument("subject", type=str, required=True, choices=choices)
-        parser.argument("text", type=helpers.string, required=True)
-        parser.argument("slug", type=str, required=True)
-        args = parser.parse()
+    parser = RequestParser()
+    parser.argument("subject", type=str, required=True, choices=choices)
+    parser.argument("text", type=helpers.string, required=True)
+    parser.argument("slug", type=str, required=True)
+    args = parser.parse()
 
-        service = get_service(args["subject"])
-        subject = service.get_by_slug(args["slug"])
+    service = get_service(args["subject"])
+    subject = service.get_by_slug(args["slug"])
 
-        comment = CommentService.create(subject, request.account, args["text"])
-        result["data"] = comment.dict()
+    comment = CommentService.create(subject, request.account, args["text"])
+    result["data"] = comment.dict()
 
-        return result
+    return result
 
-class UpdateComment(MethodView):
-    @auth_required
-    def post(self):
-        result = {"error": None, "data": {}}
+@blueprint.route("/comments/update", methods=["POST"])
+@auth_required
+def update_comment():
+    result = {"error": None, "data": {}}
 
-        parser = RequestParser()
-        parser.argument("counter", type=int, required=True)
-        parser.argument("params", type=dict, default={})
-        args = parser.parse()
+    parser = RequestParser()
+    parser.argument("counter", type=int, required=True)
+    parser.argument("params", type=dict, default={})
+    args = parser.parse()
 
-        params_parser = RequestParser()
-        params_parser.argument("text", type=helpers.string, required=True, location=("params"))
-        params_args = params_parser.parse(req=args)
+    params_parser = RequestParser()
+    params_parser.argument("text", type=helpers.string, required=True, location=("params"))
+    params_args = params_parser.parse(req=args)
 
-        comment = CommentService.get_by_counter(args["counter"], request.account)
-        if comment is None:
-            return abort("comment", "not-found")
+    comment = CommentService.get_by_counter(args["counter"], request.account)
+    if comment is None:
+        return abort("comment", "not-found")
 
-        if datetime.now() - comment.created > timedelta(minutes=20):
-            return abort("comment", "not-editable")
+    if datetime.now() - comment.created > timedelta(minutes=20):
+        return abort("comment", "not-editable")
 
-        comment.text = params_args["text"]
-        comment.updated = datetime.now()
-        comment.save()
+    comment.text = params_args["text"]
+    comment.updated = datetime.now()
+    comment.save()
 
-        result["data"] = comment.dict()
-        return result
+    result["data"] = comment.dict()
+    return result
 
-class ListComments(MethodView):
-    @auth_required
-    def post(self):
-        result = {"error": None, "data": []}
+@blueprint.route("/comments/update", methods=["POST"])
+@auth_required
+def list_comments():
+    result = {"error": None, "data": []}
 
-        parser = RequestParser()
-        parser.argument("subject", type=str, required=True, choices=choices)
-        parser.argument("slug", type=str, required=True)
-        parser.argument("page", type=int, default=0)
-        args = parser.parse()
+    parser = RequestParser()
+    parser.argument("subject", type=str, required=True, choices=choices)
+    parser.argument("slug", type=str, required=True)
+    parser.argument("page", type=int, default=0)
+    args = parser.parse()
 
-        service = get_service(args["subject"])
-        subject = service.get_by_slug(args["slug"])
-        comments = CommentService.list(subject, args["page"])
+    service = get_service(args["subject"])
+    subject = service.get_by_slug(args["slug"])
+    comments = CommentService.list(subject, args["page"])
 
-        for comment in comments:
-            result["data"].append(comment.dict())
+    for comment in comments:
+        result["data"].append(comment.dict())
 
-        return result
+    return result
