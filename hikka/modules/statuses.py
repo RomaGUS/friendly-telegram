7617d278a91f39c15ca9e71e26ddc249954fd32f
volbil <volbilnexus@gmail.com>
2020-04-19 23:28:17 +0900

    Convert statuses into blueprint

--- hikka/modules/statuses.py
+++ hikka/modules/statuses.py
@@ -1,56 +1,57 @@
 from hikka.services.statuses import StatusService
 from hikka.tools.parser import RequestParser
 from hikka.decorators import auth_required
-from flask.views import MethodView
+from flask import request, Blueprint
 from hikka.tools import helpers
-from flask import request
 from hikka import static
 
-class Update(MethodView):
-    @auth_required
-    def post(self):
-        result = {"error": None, "data": {}}
+blueprint = Blueprint("statuses", __name__)
 
-        parser = RequestParser()
-        parser.argument("rating", type=int, default=None, choices=range(1, 11))
-        parser.argument("subject", type=helpers.content, required=True)
-        parser.argument("status", type=int, default=None)
-        parser.argument("slug", type=str, default=None)
-        args = parser.parse()
+@blueprint.route("/status", methods=["POST"])
+@auth_required
+def update_status():
+    result = {"error": None, "data": {}}
 
-        subject = None
-        content = static.slug("content", args["subject"])
-        if content == "anime":
-            subject = helpers.anime(args["slug"])
+    parser = RequestParser()
+    parser.argument("rating", type=int, default=None, choices=range(1, 11))
+    parser.argument("subject", type=helpers.content, required=True)
+    parser.argument("status", type=int, default=None)
+    parser.argument("slug", type=str, default=None)
+    args = parser.parse()
 
-        status = StatusService.get(subject, request.account, args["subject"])
+    subject = None
+    content = static.slug("content", args["subject"])
+    if content == "anime":
+        subject = helpers.anime(args["slug"])
 
-        fields = ["rating", "status"]
-        for field in fields:
-            if args[field]:
-                status[field] = args[field]
+    status = StatusService.get(subject, request.account, args["subject"])
 
-        status.save()
-        result["data"] = status.dict()
+    fields = ["rating", "status"]
+    for field in fields:
+        if args[field]:
+            status[field] = args[field]
 
-        return result
+    status.save()
+    result["data"] = status.dict()
 
-class Check(MethodView):
-    @auth_required
-    def post(self):
-        result = {"error": None, "data": {}}
+    return result
 
-        parser = RequestParser()
-        parser.argument("subject", type=helpers.content, required=True)
-        parser.argument("slug", type=str, default=None)
-        args = parser.parse()
+@blueprint.route("/status/check", methods=["POST"])
+@auth_required
+def check_status():
+    result = {"error": None, "data": {}}
 
-        subject = None
-        content = static.slug("content", args["subject"])
-        if content == "anime":
-            subject = helpers.anime(args["slug"])
+    parser = RequestParser()
+    parser.argument("subject", type=helpers.content, required=True)
+    parser.argument("slug", type=str, default=None)
+    args = parser.parse()
 
-        status = StatusService.get(subject, request.account, args["subject"])
-        result["data"] = status.dict()
+    subject = None
+    content = static.slug("content", args["subject"])
+    if content == "anime":
+        subject = helpers.anime(args["slug"])
 
-        return result
+    status = StatusService.get(subject, request.account, args["subject"])
+    result["data"] = status.dict()
+
+    return result
