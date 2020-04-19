d3fa3b0243181c14d94d7ed2720fc7ee89c493a3
volbil <volbilnexus@gmail.com>
2020-04-19 22:16:36 +0900

    Convert descriptors into blueprint

--- hikka/modules/descriptors.py
+++ hikka/modules/descriptors.py
@@ -2,61 +2,63 @@ from hikka.decorators import auth_required, permission_required
 from hikka.services.descriptors import DescriptorService
 from hikka.services.func import update_document
 from hikka.tools.parser import RequestParser
-from flask.views import MethodView
 from hikka.tools import helpers
 from hikka.errors import abort
+from flask import Blueprint
 from hikka import utils
 
-class NewDescriptor(MethodView):
-    @auth_required
-    @permission_required("global", "admin")
-    def post(self):
-        result = {"error": None, "data": {}}
+blueprint = Blueprint("descriptors", __name__)
 
-        parser = RequestParser()
-        parser.argument("service", type=helpers.descriptor_service, required=True)
-        parser.argument("name", type=str, required=True)
-        parser.argument("slug", type=str, required=True)
-        parser.argument("description", type=str)
-        args = parser.parse()
+@blueprint.route("/descriptors/new", methods=["POST"])
+@auth_required
+@permission_required("global", "admin")
+def new_descriptor():
+    result = {"error": None, "data": {}}
 
-        check = DescriptorService.get_by_slug(args["service"], args["slug"])
-        if check:
-            return abort(args["service"], "slug-exists")
+    parser = RequestParser()
+    parser.argument("service", type=helpers.descriptor_service, required=True)
+    parser.argument("name", type=str, required=True)
+    parser.argument("slug", type=str, required=True)
+    parser.argument("description", type=str)
+    args = parser.parse()
 
-        descriptor = DescriptorService.create(
-            args["service"],
-            args["name"],
-            args["slug"],
-            args["description"])
+    check = DescriptorService.get_by_slug(args["service"], args["slug"])
+    if check:
+        return abort(args["service"], "slug-exists")
 
-        result["data"] = descriptor.dict()
-        return result
+    descriptor = DescriptorService.create(
+        args["service"],
+        args["name"],
+        args["slug"],
+        args["description"])
 
-class UpdateDescriptor(MethodView):
-    @auth_required
-    @permission_required("global", "admin")
-    def post(self):
-        result = {"error": None, "data": {}}
+    result["data"] = descriptor.dict()
+    return result
 
-        parser = RequestParser()
-        parser.argument("service", type=helpers.descriptor_service, required=True)
-        parser.argument("slug", type=str, required=True)
-        parser.argument("params", type=dict, default={})
-        args = parser.parse()
+@blueprint.route("/descriptors/update", methods=["POST"])
+@auth_required
+@permission_required("global", "admin")
+def update_descriptor():
+    result = {"error": None, "data": {}}
 
-        descriptor = DescriptorService.get_by_slug(args["service"], args["slug"])
-        if descriptor is None:
-            return abort(args["service"], "not-found")
+    parser = RequestParser()
+    parser.argument("service", type=helpers.descriptor_service, required=True)
+    parser.argument("slug", type=str, required=True)
+    parser.argument("params", type=dict, default={})
+    args = parser.parse()
 
-        keys = ["name", "slug", "description"]
-        update = utils.filter_dict(args["params"], keys)
-        update_document(descriptor, update)
+    descriptor = DescriptorService.get_by_slug(args["service"], args["slug"])
+    if descriptor is None:
+        return abort(args["service"], "not-found")
 
-        try:
-            descriptor.save()
-        except Exception:
-            return abort("general", "empty-required")
+    keys = ["name", "slug", "description"]
+    update = utils.filter_dict(args["params"], keys)
+    update_document(descriptor, update)
 
-        result["data"] = descriptor.dict()
-        return result
+    try:
+        descriptor.save()
+    except Exception:
+        return abort("general", "empty-required")
+
+    result["data"] = descriptor.dict()
+    return result
